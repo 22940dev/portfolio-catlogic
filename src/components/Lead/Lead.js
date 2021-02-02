1cc6c07118826fd8241121ccd476f9111d374341
Andrey Kudryavtsev <lomastering@gmail.com>
2021-02-02 23:00:47 +0300

    feat: new skills

--- src/components/Lead/Lead.js
+++ src/components/Lead/Lead.js
@@ -12,31 +12,32 @@ function Lead() {
         </h1>
         <span className="lead__hr"></span>
         <p className="lead__text">
-          Photographer, system&nbsp;administrator, technical&nbsp;editor, but,
-          most importantly - <strong>a&nbsp;JavaScript&nbsp;developer</strong>{' '}
-          based in Moscow,&nbsp;Russia.
+          A photographer, a system&nbsp;administrator, a technical&nbsp;editor,
+          but, most importantly -{' '}
+          <strong>a&nbsp;JavaScript&nbsp;developer</strong> based in
+          Moscow,&nbsp;Russia.
         </p>
         <p className="lead__text">
-          I can't fix your printer, but I'm the one you need for healing your
-          website or app sick with dirty or unoptimized code. I can find
-          solutions for all the previous bad decisions; moreover, we can start
-          from scratch to build a beautiful and innovative system design.
+          I can not fix your printer, but I can analyze problems, find solutions,
+          gentrify and optimize the code, turn your ideas into concepts, and
+          turn concepts into working websites and applications.
         </p>
         <p className="lead__text">
           My trade is a modern <strong>JavaScript</strong>, in vanilla/framework
-          form, I love doing magic with <strong>React</strong> components.
+          form, I love doing magic with <strong>React</strong> components. I am
+          focused on front-end, but also familiar with Node back-end.
         </p>
         <p className="lead__text">
-          I have no STEM background, but I'm really passionate about coding,
-          learning, and getting the best results. I'm also a team person, I like
+          I have no STEM background, but I am extremely passionate about coding,
+          learning, and getting the best results. I am also a team person, I like
           helping teammates and gaining new knowledge from a group mind.
         </p>
         <p className="lead__text">
-          I also love <strong>cats</strong>, and there's no helping it.
+          I also love <strong>cats</strong>, and there is no helping it.
         </p>
         <p className="lead__text">
-          I'm <strong>currently available</strong> for a starting-to-middle
-          position!
+          I am <strong>currently available</strong> for hire for a
+          starting-to-middle position!
         </p>
         <p className="lead__subtext">
           Check out{' '}
