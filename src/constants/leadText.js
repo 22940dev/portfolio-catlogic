b5dc81b07cce8cd42933ecf9e524d7d2441b795c
Andrey Kudryavtsev <lomastering@gmail.com>
2021-02-21 22:00:39 +0300

    refactor: lead text

---
+++ src/constants/leadText.js
@@ -0,0 +1,8 @@
+const leadText = [
+  'A <strong>React Frontend developer</strong> based in Moscow, Russia.',
+  'I cannot fix your printer, but I can analyze problems, find solutions, optimize the code, turn your ideas into concepts, and turn concepts into working websites and applications.',
+  'My tool of the trade is a modern <strong>JavaScript</strong>, in vanilla an framework form, I love doing magic with <strong>React</strong> components. I am focused on front-end, but I\'m also familiar with Node/Express back-end and Mongo databases.',
+  'I am eager to find a great team to put my knowledge, skills and expertise into a good use.',
+];
+
+export default leadText;
