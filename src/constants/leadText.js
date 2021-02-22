736a337317e3988a09d6f55a06b29e3ff7fdc86b
Andrey Kudryavtsev <lomastering@gmail.com>
2021-02-23 00:28:47 +0300

    fix: typo

--- src/constants/leadText.js
+++ src/constants/leadText.js
@@ -1,7 +1,7 @@
 const leadText = [
   'A <strong>React Frontend developer</strong> based in Moscow, Russia.',
   'I cannot fix your printer, but I can analyze problems, find solutions, optimize the code, turn your ideas into concepts, and turn concepts into working websites and applications.',
-  'My tool of the trade is a modern <strong>JavaScript</strong>, in vanilla an framework form, I love doing magic with <strong>React</strong> components. I am focused on front-end, but I\'m also familiar with Node/Express back-end and Mongo databases.',
+  'My tool of the trade is a modern <strong>JavaScript</strong>, in vanilla and framework form, I like building with <strong>React</strong> functional components. I am focused on front-end, but also familiar with Node/Express back-end and Mongo databases.',
   'I am eager to find a great team to put my knowledge, skills and expertise to a good use.',
 ];
 
