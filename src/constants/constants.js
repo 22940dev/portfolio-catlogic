05877fd77fc24b7da73de4ef09fd4ec6b17d6ae2
Andrey Kudryavtsev <lomastering@gmail.com>
2021-06-08 10:11:22 +0300

    feat: rick and morty project

--- src/constants/constants.js
+++ src/constants/constants.js
@@ -38,8 +38,6 @@ import imageClockHi from '../images/project_clock_hi.png';
 import imageClock from '../images/project_clock.png';
 import imageNewsHi from '../images/project_news-explorer_hi.png';
 import imageNews from '../images/project_news-explorer.png';
-import imageKanbanHi from '../images/project_kanban_hi.png';
-import imageKanban from '../images/project_kanban.png';
 import imageBookshelfHi from '../images/project_bookshelf_hi.png';
 import imageBookshelf from '../images/project_bookshelf.png';
 import imageAsteroidsHi from '../images/project_asteroids_hi.png';
@@ -50,15 +48,17 @@ import imageMemoryHi from '../images/project_memory_hi.png';
 import imageMemory from '../images/project_memory.png';
 import imageApartmentsHi from '../images/project_apartments_hi.png';
 import imageApartments from '../images/project_apartments.png';
+import imageMortyHi from '../images/project_rickmorty_hi.png';
+import imageMorty from '../images/project_rickmorty.png';
 
 const LEARN_STRING = 'TypeScript/Redux';
-const HEADER_STACK = ['CSS', 'TS', 'NEXT', 'REACT', 'NODE', 'GIT', 'REDUX'];
+const HEADER_STACK = ['REACT', 'SCSS', 'TS', 'GIT', 'NEXT', 'REDUX', 'NODE'];
 
 const STACK = [
   { name: 'HTML5', icon: faHtml5 },
   { name: 'CSS3', icon: faCss3Alt },
   { name: 'JavaScript / TypeScript', icon: faJs },
-  { name: 'React / Redux / Saga', icon: faReact },
+  { name: 'React / Redux', icon: faReact },
   { name: 'Next.js', icon: faStepForward },
   { name: 'Node.js / Express.js', icon: faNodeJs },
   { name: 'MongoDB', icon: faDatabase },
@@ -93,32 +93,32 @@ const SOCIAL = [
 
 const PROJECTS = [
   {
-    name: 'React Order Form',
-    stack: '(TypeScript, React, Formik, SCSS)',
-    link: 'https://cat-street.github.io/flatstack-orderform/',
-    repo: 'https://github.com/cat-street/flatstack-orderform',
-    img: imageOrder,
-    imgHi: imageOrderHi,
-  },
-  {
     name: 'React Bookshelf',
-    stack: '(TypeScript, React, Redux, Bootstrap)',
+    stack: '(React, TypeScript, Redux, Bootstrap)',
     link: 'https://zen-austin-25d292.netlify.app/',
     repo: 'https://github.com/cat-street/react-bookshelf',
     img: imageBookshelf,
     imgHi: imageBookshelfHi,
   },
   {
-    name: 'Armageddon V',
-    stack: '(TypeScript, React, Next.js)',
-    link: 'https://asteroids-nasa.vercel.app/',
-    repo: 'https://github.com/cat-street/asteroids-nasa',
-    img: imageAsteroids,
-    imgHi: imageAsteroidsHi,
+    name: 'React Order Form',
+    stack: '(React, TypeScript, Formik, SCSS)',
+    link: 'https://cat-street.github.io/flatstack-orderform/',
+    repo: 'https://github.com/cat-street/flatstack-orderform',
+    img: imageOrder,
+    imgHi: imageOrderHi,
+  },
+  {
+    name: 'Rick and Morty App',
+    stack: '(React, TypeScript, React-query, Material UI)',
+    link: 'https://thirsty-allen-60674b.netlify.app/',
+    repo: 'https://github.com/cat-street/rick-and-morty',
+    img: imageMorty,
+    imgHi: imageMortyHi,
   },
   {
     name: 'Smooth Apartments',
-    stack: '(React, Redux, SCSS)',
+    stack: '(React, TypeScript, Redux, SCSS)',
     link: 'https://cat-street.github.io/creative-apartments/',
     repo: 'https://github.com/cat-street/creative-apartments',
     img: imageApartments,
@@ -126,7 +126,7 @@ const PROJECTS = [
   },
   {
     name: 'Memory Game',
-    stack: '(React, TypeScript)',
+    stack: '(React, TypeScript, SCSS)',
     link: 'https://cat-street.github.io/creative-memory-game/',
     repo: 'https://github.com/cat-street/creative-memory-game',
     img: imageMemory,
@@ -134,19 +134,19 @@ const PROJECTS = [
   },
   {
     name: 'News Explorer',
-    stack: '(JavaScript, React, Node/Express, MongoDB)',
+    stack: '(React, JavaScript, Node/Express, MongoDB)',
     link: 'https://news.catlogic.ru/',
     repo: 'https://github.com/cat-street/news-explorer',
     img: imageNews,
     imgHi: imageNewsHi,
   },
   {
-    name: 'Sample Kanban board',
-    stack: '(TypeScript, React, Bootstrap)',
-    link: 'https://cat-street.github.io/anyway-kanban/',
-    repo: 'https://github.com/cat-street/anyway-kanban',
-    img: imageKanban,
-    imgHi: imageKanbanHi,
+    name: 'Armageddon V',
+    stack: '(React, TypeScript, Next.js)',
+    link: 'https://asteroids-nasa.vercel.app/',
+    repo: 'https://github.com/cat-street/asteroids-nasa',
+    img: imageAsteroids,
+    imgHi: imageAsteroidsHi,
   },
   {
     name: 'CatLogic Calculator',
